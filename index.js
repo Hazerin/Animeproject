71b72dba7f909e53e78d9a92e7823f87a7166605
Hazerin <alby91v@gmail.com>
2023-08-13 16:34:26 +0200

    ripresa del progetto

--- index.js
+++ index.js
@@ -35,7 +35,13 @@ let displayValThree = document.getElementById(`range3`);
 let sliderThree = document.getElementById(`myRange3`);
 let sliderTrack2 = document.querySelector(`.slider__track--2`);
 
-let genres;
+/* Variabile che mi serve per poi usare il contenuto della casella genere per la ricerca tramite API */
+let genrevalue;
+
+/* Per le stelle */
+let rating;
+
+let anime = document.getElementById(`animelist`)
 
 function autocomplete(inp, arr) {
   /*Argomenti: campo di testo e array di valori "autocompletati"*/
@@ -45,42 +51,45 @@ function autocomplete(inp, arr) {
   funzione esterna mettendo quella come secondo parametro. */
   /* L'addEventListener fa si che quando si registri un determinato tipo di "input" (come i click) e una
   funzione da eseguire quando si interagisce con un determinato elemento della pagina. */
-  inp.addEventListener("input", function() {
-      /* this.value = valore dell'input */
-      var a, b, i, val = this.value;
-      /*close any already open lists of autocompleted values*/
-      closeAllLists();
-      if (!val) {return false}
-      currentFocus = -1;
-      /*create a DIV element that will contain the items (values):*/
-      a = document.createElement("DIV");
-      a.setAttribute("id", this.id + "autocomplete-list");
-      a.setAttribute("class", "autocomplete-items");
-      /*append the DIV element as a child of the autocomplete container:*/
-      this.parentNode.appendChild(a);
-      /*for each item in the array...*/
-      for (i = 0; i < arr.length; i++) {
-        /*check if the item starts with the same letters as the text field value:*/
-        if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
-          /*create a DIV element for each matching element:*/
-          b = document.createElement("DIV");
-          /*make the matching letters bold:*/
-          b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
-          b.innerHTML += arr[i].substr(val.length);
-          /*insert a input field that will hold the current array item's value:*/
-          b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
-          /*execute a function when someone clicks on the item value (DIV element):*/
-              b.addEventListener("click", function(e) {
-              /*insert the value for the autocomplete text field:*/
-              inp.value = this.getElementsByTagName("input")[0].value;
-              /*close the list of autocompleted values,
-              (or any other open lists of autocompleted values:*/
-              closeAllLists();
-          });
-          a.appendChild(b);
-        }
+  inp.addEventListener("input", function(e) {
+    genrevalue = inp.value;
+    /* this.value = valore dell'input */
+    var a, b, i, val = this.value;
+    /*close any already open lists of autocompleted values*/
+    closeAllLists();
+    if (!val) {return false}
+    currentFocus = -1;
+    /*create a DIV element that will contain the items (values):*/
+    a = document.createElement("DIV");
+    a.setAttribute("id", this.id + "autocomplete-list");
+    a.setAttribute("class", "autocomplete-items");
+    /*append the DIV element as a child of the autocomplete container:*/
+    this.parentNode.appendChild(a);
+    /*for each item in the array...*/
+    for (i = 0; i < arr.length; i++) {
+      /*check if the item starts with the same letters as the text field value:*/
+      if (arr[i].substr(0, val.length).toUpperCase() == val.toUpperCase()) {
+        /*create a DIV element for each matching element:*/
+        b = document.createElement("DIV");
+        /*make the matching letters bold:*/
+        b.innerHTML = "<strong>" + arr[i].substr(0, val.length) + "</strong>";
+        b.innerHTML += arr[i].substr(val.length);
+        /*insert a input field that will hold the current array item's value:*/
+        b.innerHTML += "<input type='hidden' value='" + arr[i] + "'>";
+        /*execute a function when someone clicks on the item value (DIV element):*/
+            b.addEventListener("click", function(e) {
+            /*insert the value for the autocomplete text field:*/
+            inp.value = this.getElementsByTagName("input")[0].value;
+            /*close the list of autocompleted values,
+            (or any other open lists of autocompleted values:*/
+            closeAllLists();
+            genrevalue = inp.value;
+        });
+        a.appendChild(b);
       }
+    }
   });
+
   /*execute a function presses a key on the keyboard:*/
   inp.addEventListener("keydown", function(e) {
       var x = document.getElementById(this.id + "autocomplete-list");
@@ -133,7 +142,7 @@ function autocomplete(inp, arr) {
   }
 }
   /*execute a function when someone clicks in the document:*/
-  document.addEventListener("click", function (e) {
+  document.addEventListener("click", function (e){
       closeAllLists(e.target);
   });
   }
@@ -142,17 +151,18 @@ function slideOne(){
     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
     sliderOne.value = parseInt(sliderTwo.value) - minGap;
     }
-    displayValOne.textContent = 240 - sliderOne.value;
+    displayValOne.textContent = parseInt((240 - sliderOne.value)/12) + " y" + " " + (240 - sliderOne.value) % 12 + "  m";
     fillColor();
     if (parseInt(sliderOne.value) === 0) {
       displayValOne.textContent = `Any date`
     }
 }
+
 function slideTwo(){
     if(parseInt(sliderTwo.value) - parseInt(sliderOne.value) <= minGap) {
         sliderTwo.value = parseInt(sliderOne.value) + minGap;
     }
-    displayValTwo.textContent = 240 - sliderTwo.value;
+    displayValTwo.textContent = parseInt((240 - sliderTwo.value)/12) + " y" + " " + (240 - sliderTwo.value) % 12 + "  m";
     fillColor();
     if (parseInt(sliderTwo.value) === 240) {
       displayValTwo.textContent = `This month`
@@ -182,21 +192,247 @@ function fillColor2() {
     #dadae5 ${percent1}%, #dadae5 100%)`
 }
 
+function litstar(halfstars) {
+  let fullstars = parseInt(halfstars / 2);
+  let stars = document.querySelector(".stars");
+  stars.innerHTML = ``;
+
+  for (let i = 0; i < 5; i++) {
+
+    if (i < fullstars) {
+    stars.innerHTML += `
+      <div class="half__star--left click star1" onclick="litstar(${i*2+1})">
+      <img class="star" src="https://upload.wikimedia.org/wikipedia/commons/e/e5/Full_Star_Yellow.svg" alt=""></div>
+      <div class="half__star--right click star2" onclick="litstar(${i*2+2})"></div>`
+    }
+
+    if (i === fullstars && halfstars % 2 !== 0) {
+    stars.innerHTML += `
+      <div class="half__star--left click star1" onclick="litstar(${i*2+1})">
+      <img class="star" src="https://upload.wikimedia.org/wikipedia/commons/d/d6/Half_Star_Yellow.svg" alt=""></div>
+      <div class="half__star--right click star2" onclick="litstar(${i*2+2})"></div>`
+    }
+
+    if (i === fullstars && halfstars % 2 === 0) {
+      stars.innerHTML += `
+      <div class="half__star--left click star1" onclick="litstar(${i*2+1})">
+      <img class="star" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Empty_Star.svg" alt=""></div>
+      <div class="half__star--right click star2" onclick="litstar(${i*2+2})"></div>`
+    }
+
+    if (i > fullstars) {
+      stars.innerHTML += `
+      <div class="half__star--left click star1" onclick="litstar(${i*2+1})">
+      <img class="star" src="https://upload.wikimedia.org/wikipedia/commons/e/e7/Empty_Star.svg" alt=""></div>
+      <div class="half__star--right click star2" onclick="litstar(${i*2+2})"></div>`
+    }
+
+  }
+
+  document.querySelector(".numeric_rating").textContent = `${halfstars}`;
+
+  rating = halfstars;
+
+}
+
 async function main() {
     const post = await fetch(`https://api.jikan.moe/v4/genres/anime`);
-    const postjson = await post.json();
+    let postjson = await post.json();
     console.log(postjson);
-    genres = postjson.data.map(x => {
+    postjson = postjson.data.map(x => {
         return x.name;
     })
-    console.log(genres);
+    console.log(postjson);
     /* Devo aspettare l'async mettendo dentro pure la funzione che inizializza l'autocomplete o far aspettare fuori in qualche modo */
-    autocomplete(document.getElementById("search-genres"), genres);
+    autocomplete(document.getElementById("search-genres"), postjson);
 }
 
 main();
 
-/* let arr = [1, 4, 9 , 16]
+/* miao è l'handler della promise, diverso dalla funzioe miao esterna, in questo caso.
+Chiamandola quindi in verità la funzione miao non fa altro che impostare l'handler con
+"resolved" considerando quindi la promise risolta. */
+async function sleep(time) {
+  await new Promise (miao => setTimeout(miao, time));
+}
+
+/* function miao() {
+  console.log("miao");
+} */
+
+/* Qui la funzione viene chiamata ma hello, ovvero il parametro per verificare se la promise è risolta, rimane
+undefined causando lo scorretto funzionamento del programma o il blocco. */
+/* async function sleep(time) {
+  await new Promise (hello => setTimeout(miao, time));
+} */
+
+/* Versione corretta. ogni iterazione di Timeout imposta anche la variabile hello a "resolved" in quanto
+viene raggiunta correttamente dalla funzione. */
+/* async function sleep(time) {
+  await new Promise (hello => setTimeout(() => {
+    miao();
+    hello();
+  }, time));
+} */
+
+let post = [];
+
+/* Non si può usare il metodo json su un array */
+async function updateAnime() {
+  /* intendo fare un array in cui tutti gli spazi siano occupati da dati utili */
+  let fillcount = 1;
+
+  for (let i = 1; i < 50000; i++) {
+    let temp = await fetch(`https://api.jikan.moe/v4/anime/${i}/full`);
+
+    if (temp.status === 404) {
+      fillcount++;
+    }
+    else
+    post[i-fillcount] = await temp.json();
+
+    if (temp.status === 429) {
+      await sleep(500);
+      i--;
+    }
+
+  }
+  console.log(post);
+}
+
+function download(content, fileName, contentType) {
+  /* Crea un nuovo elemento. il nome è la categoria di elemento HTML (a perchè supporta il download) */
+  const a = document.createElement(`a`);
+  console.log(a);
+  /* i parametri sono l'array di stringhe e il tipo di contenuto è text/plain */
+  const file = new Blob([content], {type: contentType});
+  a.href = URL.createObjectURL(file);
+  a.download = fileName;
+  /* click eseguito dalla sull'elemento per avviare il download */
+  a.click();
+}
+
+function ondownload() {
+  download(JSON.stringify(post), `AllAnime.json`, `text/plain`)
+}
+
+/* Gestione degli overflow */
+
+
+
+/* Ricerca degli anime */
+
+function searchAnime() {
+
+  let laterDate = 240 - sliderOne.value;
+  if (laterDate === 240) {
+    laterDate = 9999999;
+  }
+  const recentDate = Math.abs(sliderTwo.value - 240);
+  const genre = genrevalue;
+  const episodes = sliderThree.value;
+  const stars = rating;
+  const title = document.getElementById("search_bar").value;
+
+  console.log(laterDate);
+  console.log(recentDate);
+  console.log(genre);
+  console.log(episodes);
+  console.log(stars);
+  console.log(title);
+
+  let animelist = document.querySelector(`#animelist`);
+  animelist.innerHTML = ``;
+
+  /* Numero di elementi HTML massimo da generare */
+  let count = 0;
+
+  for (let i = 0; i < post.length; i++) {
+    if (count >= 20) {
+      break;
+    }
+    if (monthsAgo(post[i].data.aired.from) > recentDate && monthsAgo(post[i].data.aired.from) < laterDate)  {
+    animelist.innerHTML += `<div class="anime">
+    <img class="anime__poster" src="${post[i].data.images.jpg.large_image_url}" alt=""> 
+    <div class="anime__title">
+    <p class="anime__title--text">${post[i].data.title}</p></div></div>`;
+    count++;
+    }
+  }
+
+  /* Funzione che verrà chiamata quando il contenuto di un elemento supera i limiti dell'elemento stesso
+  Se la percentuale di intersezione tra un elemento e l'altro è minore di 1 (100%) con la funzione
+  intersectionRatio vuol dire che c'è un overflow */
+
+  function handleOverflow(entries) {
+    entries.forEach(x => {
+      if (x.intersectionRatio < 1) {
+        console.log("Overflow");
+      }
+    })
+  }
+
+  var options = {
+    /* devo selezionare tutti i genitori, qui ne è selezionato uno solo */
+    root: document.querySelector(`.anime__title`),
+    rootMargin: "0px",
+    /* se anche un solo pixel è visibile, l'elemento viene valutato dall'observer. Utile in questo caso
+    perchè l'overflow della sinossi potrebbe essere molto lungo. 1 vuol dire che ogni singolo
+    pixel dell'elemento osservato dev'essere visibile nella viewport (in questo caso la viewport è il padre) */
+    threshold: 0
+  }
+
+  // Crea un observer per monitorare gli elementi .anime__title
+  const observer = new IntersectionObserver(handleOverflow, options);
+
+  // Seleziona tutti gli elementi .anime__title
+  var animeTitles = document.querySelectorAll('.anime__title--text');
+
+  /* Registra l'observer per ciascun elemento .anime__title
+  map va usato quando bisogna ritornare un nuovo array, forEach se non c'è da ritornare nulla! */
+  animeTitles.forEach(title => {
+    observer.observe(title);
+  });
+
+/* map itera per forza su ogni elemento, soluzione scartata. */
+/*   post.map(x => {
+    if (count >= 20) {
+      console.log("a");
+      return;
+    }
+    console.log(x.data.episodes);
+  }); */
+}
+
+/* Funzioni di conversione dei dati */
+
+function monthsAgo(date) {
+  const today = new Date();
+  /* Converte in stringa e assicura che mese e giorno siano a due cifre */
+  const amg = today.getFullYear() + today.getMonth().toString().padStart(2,`0`) +
+  today.getDate().toString().padStart(2,`0`);
+  /* Funzione slice: taglia una stringa tra due intervalli */
+  /* Sostituisce tutti i caratteri corrispondenti a quello o al range di quelli indicati
+  tra i due slash con quello indicato nel secondo parametro della funzione replace. */
+  date = date.slice(0,10).replace(/-/g,``);
+  /* Leading o trailing, sono sequenze a inizio o fine stringa.
+  "aaa eee uuu" le 3 a sono leading e le 3 u sono trailing. Commento a scopo didattico, non sto
+  usando questi concetti in questo codice.*/
+  ///////////////////////////////////////////////////
+  /* Calcolo di quanti mesi fa è uscito l'anime VS i mesi inseriti nei parametri */
+  let datevalue = parseInt(date.slice(0,4)) * 12 + parseInt(date.slice(4,6));
+  return (parseInt(amg.slice(0,4) * 12 + parseInt(amg.slice(4,6))) - datevalue);
+}
+
+testpost();
+
+async function testpost() {
+  post = await fetch(`./Resources/AllAnime.json`);
+  post = await post.json();
+  console.log(post);
+}
+
+/* let arr = [1, 4, 9, 16]
 
 arr.map((elem) => {
     console.log(elem)
